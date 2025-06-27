31595b2293fb519c6166398f4f069ed94f072364
Team <teammembers@qadigitalads.com>
2025-06-27 11:11:42 -0500

    Button updated

--- src/app/components/animatedLine.jsx
+++ src/app/components/animatedLine.jsx
@@ -53,6 +53,13 @@ export default function AnimatedLine({classes, children}) {
       <svg xmlns="http://www.w3.org/2000/svg" width="560" height="212" viewBox="0 0 560 212" fill="none" style={{transform: 'rotate(-15.3deg)'}} className="mt-[-80px] lg:ms-[-600px] ms-[-380px] w-[360px] lg:w-full">
           <path d="M-199.088 0.543211C-135.746 17.5921 -72.4615 34.828 -9.22652 52.2495C54.0219 69.6317 117.22 87.1833 180.409 104.766C243.585 122.405 306.748 140.083 369.866 157.929C432.994 175.733 496.069 193.726 559.092 211.909C495.752 194.866 432.461 177.632 369.228 160.207C305.98 142.824 242.782 125.273 179.592 107.684C116.418 90.0412 53.2553 72.3634 -9.86298 54.5177C-72.9908 36.7135 -136.066 18.7199 -199.092 0.540868L-199.088 0.543211Z" fill="white"/>
       </svg>
+{/*    <div className={`absolute lg:w-[400px] w-full h-[10px] z-10 lg:ms-[-340px] lg:mt-[50px]`}
+          style={{
+            background: 'linear-gradient(3deg, #F00 -57%, rgba(255, 0, 0, 0.00) 72.14%);',
+            backgroundBlendMode: 'screen',
+            mixBlendMode: 'screen',
+          }}
+        /> */} 
   </div>
 </div>
   );
--- src/app/components/asideMenu.jsx
+++ src/app/components/asideMenu.jsx
@@ -75,10 +75,10 @@ export default function ScrollSpyMenu() {
         </div>
       </div>
 
-      <div className="mt-6 pr-2">
+      <div className="mt-6">
         <a
           href="#quote"
-          className="bg-red-600 text-white px-4 py-2 font-bold shadow-md transform -rotate-2 inline-block"
+          className="bg-red-600 text-white px-4 py-2 font-bold shadow-md transform inline-block"
         >
           Get a quote now!
         </a>
